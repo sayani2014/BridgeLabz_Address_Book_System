208e102b0100d3f012eb6eddf4af2cd6d17f039c
Sayani Koley <sayanikoley8@gmail.com>
2021-06-15 10:57:54 +0530

    Added Welcome Message to the Program

---
+++ AddressBookSystem.java
@@ -0,0 +1,12 @@
+/**
+ * Display Welcome to Address Book
+ *
+ * @author: SAYANI KOLEY
+ * @since: 15/06/2021
+ */
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+    }
+}
